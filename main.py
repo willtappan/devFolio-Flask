2daa23b300abb53bb44627ce34d59bcfa66fb062
William <ing.tappan@gmail.com>
2022-09-02 21:13:14 -0500

    app run config

--- main.py
+++ main.py
@@ -20,4 +20,4 @@ def login():
 
 if __name__ == "__main__":
     #verDebug = True if (os.environ.get('Debug', "True")) == "True") else  False
-    app.run(debug=True,host= '127.0.0.1' , port=8080)
+    app.run(debug=False,host= '0.0.0.0' , port=8080)

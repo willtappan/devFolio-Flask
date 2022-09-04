1bc89ec1bdaa246109870009be76c059fbb4d0d0
William <ing.tappan@gmail.com>
2022-09-04 15:31:32 -0500

    subiendo cambios de edicion

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ def login():
     return render_template('login.html')
 
 
-
 if __name__ == "__main__":
     #verDebug = True if (os.environ.get('Debug', "True")) == "True") else  False
     app.run(debug=False,host= '0.0.0.0' , port=8080)
